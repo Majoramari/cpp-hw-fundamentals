29edb5528cf056f6d59169d28ef7d8ee663b3ffa
MajorAmari <major.amari@gmail.com>
2024-09-09 11:27:36 +0300

    keep the main file to bare minimum

--- main.cpp
+++ main.cpp
@@ -5,9 +5,4 @@
 using namespace std;
 
 int main() {
-	const int number = utils::get_number();
-
-	const string result = number == 0 ? "Zero" : number > 0 ? "Positive" : "Negative";
-
-	cout << "The number is " << number << " is " << result << endl;
 }
