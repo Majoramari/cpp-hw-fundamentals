28084f99eb3da8ee494edc2cc60f4725a3bbd6a9
MajorAmari <jmari@duck.com>
2024-09-11 13:16:54 +0300

    solve #6-30 homework
    Signed-off-by: MajorAmari <jmari@duck.com>

--- main.cpp
+++ main.cpp
@@ -1,8 +1,59 @@
 #include <iostream>
+#include <vector>
 
 #include "utils.h"
 
 using namespace std;
 
+struct Employee {
+	string first_name;
+	string last_name;
+	int salary{};
+};
+
+void read_employees(vector<Employee> &employees) {
+	bool add_more_employees = true;
+
+	while (add_more_employees) {
+		Employee employee;
+
+		cout << "Enter employee first name: ";
+		cin >> employee.first_name;
+		cout << "Enter employee last name: ";
+		cin >> employee.last_name;
+
+		cout << "Enter employee salary: ";
+		while (!(cin >> employee.salary) || employee.salary < 0) {
+			cout << "Invalid input! Please enter a valid salary: ";
+			cin.clear();
+			cin.ignore(numeric_limits<streamsize>::max(), '\n');
+		}
+
+		employees.push_back(employee);
+
+		char user_input;
+		cout << "\nDo you want to add another employee? (y/n): ";
+		cin >> user_input;
+		user_input = tolower(user_input);
+		cout << endl;
+
+		if (user_input != 'y')
+			add_more_employees = false;
+	}
+}
+
+void print_employees(vector<Employee> &employees) {
+	for (auto &[first_name, last_name, salary]: employees) {
+		cout << "Employee's first name: " << first_name << endl;
+		cout << "Employee's last name: " << last_name << endl;
+		cout << "Employee's salary: " << salary << endl;
+		cout << endl;
+	}
+}
+
 int main() {
+	vector<Employee> employees;
+
+	read_employees(employees);
+	print_employees(employees);
 }
