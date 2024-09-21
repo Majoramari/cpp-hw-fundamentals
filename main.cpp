370cee3015fbe0fa01f7a674b4716449afdd87af
MajorAmari <think-basin-amber@duck.com>
2024-09-21 19:29:08 +0300

    problem#40
    Signed-off-by: MajorAmari <think-basin-amber@duck.com>

--- main.cpp
+++ main.cpp
@@ -13,11 +13,27 @@ string join_string(vector<string> strings, string delimiter) {
 	return result.substr(0, result.length() - delimiter.length());
 }
 
+string join_string(string arr[100], const int arr_length, string delimiter) {
+	string result;
+
+	for (int i = 0; i < arr_length; i++) {
+		result += arr[i] + delimiter;
+	}
+
+	return result.substr(0, result.length() - delimiter.length());
+}
+
 int main() {
-	vector<string> strings = {"Muhannad", "Hassan", "Elbolaky"};
+	const vector<string> vector_string = {"Muhannad", "Hassan", "Elbolaky"};
+	string arr[100]{"Muhannad", "Hassan", "Elbolaky"};
 
 	cout << "Vector after join: " << endl;
-	cout << join_string(strings, ", ") << endl;
+	cout << join_string(vector_string, ", ") << endl;
+
+	cout << endl;
+
+	cout << "Array after join: " << endl;
+	cout << join_string(arr, 3, ", ") << endl;
 
 	return 0;
 }
