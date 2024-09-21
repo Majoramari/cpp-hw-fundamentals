6dc1f45525350444de844680de9849eedb037191
MajorAmari <think-basin-amber@duck.com>
2024-09-21 19:20:06 +0300

    problem#39
    Signed-off-by: MajorAmari <think-basin-amber@duck.com>

--- main.cpp
+++ main.cpp
@@ -1,7 +1,23 @@
-#include <iomanip>
+# include <iostream>
+#include <vector>
 
 using namespace std;
 
+string join_string(vector<string> strings, string delimiter) {
+	string result;
+
+	for (string &s: strings) {
+		result += s + delimiter;
+	}
+
+	return result.substr(0, result.length() - delimiter.length());
+}
+
 int main() {
+	vector<string> strings = {"Muhannad", "Hassan", "Elbolaky"};
+
+	cout << "Vector after join: " << endl;
+	cout << join_string(strings, ", ") << endl;
+
 	return 0;
 }
