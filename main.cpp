b90445339d651631271d87ef72dc83062ebae226
MajorAmari <think-basin-amber@duck.com>
2024-09-22 05:53:43 +0300

    problem#47
    Signed-off-by: MajorAmari <think-basin-amber@duck.com>

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
@@ -30,7 +31,24 @@ Client create_client() {
 	return client;
 }
 
-string convert_record_to_line(const Client &client, const string &separator) {
+vector<string> split(const string &str, const string &delimiter) {
+	vector<string> tokens;
+	string::size_type start = 0, end;
+
+	while ((end = str.find(delimiter, start)) != string::npos) {
+		if (string token = str.substr(start, end - start); !token.empty())
+			tokens.push_back(token);
+
+		start = end + delimiter.length();
+	}
+
+	if (start < str.length())
+		tokens.push_back(str.substr(start));
+
+	return tokens;
+}
+
+string convert_from_record(const Client &client, const string &separator) {
 	string line;
 
 	line += client.name + separator;
@@ -42,12 +60,33 @@ string convert_record_to_line(const Client &client, const string &separator) {
 	return line;
 }
 
+Client convert_from_line(const string &line, const string &separator) {
+	Client client;
+	vector<string> client_data = split(line, separator);
+
+	client.name = client_data[0];
+	client.account_number = stoi(client_data[1]);
+	client.pin_code = stoi(client_data[2]);
+	client.phone_number = stoi(client_data[3]);
+	client.balance = stof(client_data[4]);
+
+	return client;
+}
+
+void print_client_data(const Client &client) {
+	cout << "Name: " << client.name << endl;
+	cout << "Account: " << client.account_number << endl;
+	cout << "Pin: " << client.pin_code << endl;
+	cout << "Phone: " << client.phone_number << endl;
+	cout << "Balance: " << client.balance << endl;
+}
+
 int main() {
-	cout << "Please enter client data: " << endl;
+	const string line = "Muhannad#//#512#//#2848#//#10966#//#200.000000";
 
-	Client client = create_client();
+	const Client client = convert_from_line(line, "#//#");
 
-	cout << convert_record_to_line(client, "#//#") << endl;
+	print_client_data(client);
 
 	return 0;
 }
