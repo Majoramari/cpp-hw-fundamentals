8f0f2cc12232e0ca8fc8c597fa09d6fbb67d356c
MajorAmari <think-basin-amber@duck.com>
2024-09-22 03:39:43 +0300

    problem#44
    Signed-off-by: MajorAmari <think-basin-amber@duck.com>

--- main.cpp
+++ main.cpp
@@ -2,6 +2,19 @@
 
 using namespace std;
 
+string remove_punctuation(const string &str) {
+	string result;
+	for (const char c: str) {
+		if (!ispunct(c))
+			result += c;
+	}
+	return result;
+}
+
 int main() {
+	const string str = "Hello, World! Welcome to C++ programming.";
+
+	cout << remove_punctuation(str) << endl;
+
 	return 0;
 }
