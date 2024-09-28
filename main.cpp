a8c6be602c8af3993e9755f26f06708f189c80ab
Muhannad Elbolaky <think-basin-amber@duck.com>
2024-09-28 10:10:23 +0300

    improve performance

--- main.cpp
+++ main.cpp
@@ -4,12 +4,12 @@
 
 using namespace std;
 
-bool is_leap_year(const int &year) {
+bool is_leap_year(const unsigned short int &year) {
 	return (year % 4 == 0 && year % 100 != 0) || year % 400 == 0;
 }
 
 int main() {
-	const int year = utils::get_number("Please enter a year: ");
+	const unsigned short int year = utils::get_number("Please enter a year: ");
 
 	cout << year << (is_leap_year(year) ? " is a Leap Year" : " is not a Leap Year");
 
