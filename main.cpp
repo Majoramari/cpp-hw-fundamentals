eb1f47773af5ae3eac591285b0edbebe955fd9ae
Muhannad Elbolaky <think-basin-amber@duck.com>
2024-09-28 12:45:54 +0300

    solved

--- main.cpp
+++ main.cpp
@@ -4,61 +4,53 @@
 
 using namespace std;
 
-bool is_leap_year(const unsigned short int &year) {
-	return (year % 4 == 0 && year % 100 != 0) || year % 400 == 0;
-}
-
-unsigned short int read_month_number() {
-	unsigned short int month;
+unsigned short read_month_number() {
+	unsigned short int month = 1;
 	do {
+		if (month < 1 || month > 12)
+			cout << endl << "Invalid month number, try again: " << endl;;
+
 		cout << "Please enter a month number: ";
 		cin >> month;
 	} while (month < 1 || month > 12);
 	return month;
 }
 
-int get_month_days(const unsigned short int &year, const unsigned short int &month) {
-	static const int days_in_month[] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+unsigned short read_day_number() {
+	unsigned short int day = 1;
+	do {
+		if (day < 1 || day > 31)
+			cout << endl << "Invalid day number, try again: " << endl;;
 
-	return month == 2 && is_leap_year(year) ? 29 : days_in_month[month - 1];
+		cout << "Please enter a day number: ";
+		cin >> day;
+	} while (day < 1 || day > 31);
+	return day;
 }
 
-int get_days_in_year(const unsigned short int &year) {
-	return is_leap_year(year) ? 366 : 365;
+unsigned short calc_day_order(const unsigned short &day, const unsigned short &month, const unsigned short &year) {
+	const unsigned short a = (14 - month) / 12;
+	const unsigned short y = year - a;
+	const unsigned short m = month + 12 * a - 2;
+
+	return (day + y + y / 4 - y / 100 + y / 400 + (31 * m) / 12) % 7;
 }
 
-int get_hours_in_year(const unsigned short int &year) {
-	return get_days_in_year(year) * 24;
-}
+string get_day_name(const unsigned short &day_index) {
+	string days_of_week[] = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
 
-int get_minutes_in_year(const unsigned short int &year) {
-	return get_hours_in_year(year) * 60;
-}
-
-int get_seconds_in_year(const unsigned short int &year) {
-	return get_minutes_in_year(year) * 60;
-}
-
-// Unused in this problem
-void print_year_duration(const unsigned short int &year) {
-	cout << "Number of Days: " << get_days_in_year(year) << endl;
-	cout << "Number of Hours: " << get_hours_in_year(year) << endl;
-	cout << "Number of Minutes: " << get_minutes_in_year(year) << endl;
-	cout << "Number of Seconds: " << get_seconds_in_year(year) << endl;
-}
-
-void print_month_duration(const unsigned short int &year, const unsigned short int &month) {
-	cout << "Number of Days: " << get_month_days(year, month) << endl;
-	cout << "Number of Hours: " << get_month_days(year, month) * 24 << endl;
-	cout << "Number of Minutes: " << get_month_days(year, month) * 24 * 60 << endl;
-	cout << "Number of Seconds: " << get_month_days(year, month) * 24 * 60 * 60 << endl;
+	return days_of_week[day_index];
 }
 
 int main() {
-	const unsigned short int year = utils::get_number("Please enter a year to check: ");
-	const unsigned short int month = read_month_number();
+	const unsigned short year = utils::get_number("Please enter a year to check: ");
+	const unsigned short month = read_month_number();
+	const unsigned short day = read_day_number();
 
-	print_month_duration(year, month);
+
+	cout << "Date: " << day << "/" << month << "/" << year << endl;
+	cout << "Day order: " << calc_day_order(day, month, year) + 1 << endl;
+	cout << "Day name: " << get_day_name(calc_day_order(day, month, year)) << endl;
 
 	return 0;
 }
