42ab5800def9b944b682fe9d4f93f99464860f79
Muhannad Elbolaky <think-basin-amber@duck.com>
2024-10-05 11:46:58 +0300

    done

--- main.cpp
+++ main.cpp
@@ -1,7 +1,19 @@
-#include <iomanip>
+#include <iostream>
 
 using namespace std;
 
+string read_name() {
+	string name;
+	cout << "Please enter your name: ";
+	getline(cin, name);
+	return name;
+}
+
+void print_name(const string &name) {
+	cout << "Hello, " << name << "!" << endl;
+}
+
 int main() {
+	print_name(read_name());
 	return 0;
 }
