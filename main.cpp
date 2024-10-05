8dcadf8116ccf7b3763f0eed68702a8e77128ddf
Muhannad Elbolaky <think-basin-amber@duck.com>
2024-10-05 12:13:31 +0300

    done

--- main.cpp
+++ main.cpp
@@ -2,18 +2,22 @@
 
 using namespace std;
 
-string read_name() {
-	string name;
-	cout << "Please enter your name: ";
-	getline(cin, name);
-	return name;
+int read_number() {
+	int number;
+	cout << "Enter a number: ";
+	cin >> number;
+	return number;
 }
 
-void print_name(const string &name) {
-	cout << "Hello, " << name << "!" << endl;
+bool is_even(const int &number) {
+	return number % 2 == 0;
+}
+
+void print_number_type(const int &number) {
+	cout << "The number is: " << (is_even(number) ? "Even" : "Odd") << endl;
 }
 
 int main() {
-	print_name(read_name());
+	print_number_type(read_number());
 	return 0;
 }
