e282b08455c9c0b6a857155f2590eae61d9d6f37
Muhannad Elbolaky <think-basin-amber@duck.com>
2024-10-06 16:08:02 +0300

    done

--- main.cpp
+++ main.cpp
@@ -2,32 +2,22 @@
 
 using namespace std;
 
-struct Application {
-	int age;
-	bool has_driver_license;
-	bool has_recommendation;
-};
-
-Application read_applicant_data() {
-	Application app{};
-	cout << "How old are you?: ";
-	cin >> app.age;
-	cout << "Do you have a driver license? (1 for yes, 0 for no): ";
-	cin >> app.has_driver_license;
-	cout << "Do you have a recommendation? (1 for yes, 0 for no): ";
-	cin >> app.has_recommendation;
-	return app;
+int get_number() {
+	int number;
+	cout << "Enter a number: ";
+	cin >> number;
+	return number;
 }
 
-void check_approval_criteria(const Application &app) {
-	if ((app.age >= 21 && app.has_driver_license) || app.has_recommendation)
-		cout << "Application approved!" << endl;
-	else
-		cout << "Application denied..." << endl;
+float display_half(const int &number) {
+	return static_cast<float>(number) / 2;
+}
+
+void print_number(const int &number) {
+	cout << display_half(number) << endl;
 }
 
 int main() {
-	const Application app = read_applicant_data();
-	check_approval_criteria(app);
+	print_number(get_number());
 	return 0;
 }
