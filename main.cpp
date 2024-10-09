47a74f51f56affcc0e96d43bfaf582b86fae8a33
Muhannad Elbolaky <think-basin-amber@duck.com>
2024-10-09 18:33:52 +0300

    done

--- main.cpp
+++ main.cpp
@@ -7,24 +7,24 @@ using namespace std;
 using namespace m_date;
 
 int main() {
-	cout << "Vacation starts" << endl;
+	cout << "Starting period" << endl;
 	const Date start_date = read_date();
 
-	cout << "Vacation ends" << endl;
+	cout << "Ending period" << endl;
 	const Date end_date = read_date();
 
-	cout << "Vacation from: " << get_week_short_name(start_date.day) << ", "
+	cout << "Period from: " << get_week_short_name(start_date.day) << ", "
 			<< start_date.day << "/"
 			<< start_date.month << "/"
 			<< start_date.year << endl;
 
-	cout << "Vacation to: " << get_week_short_name(end_date.day) << ", "
+	cout << "Period to: " << get_week_short_name(end_date.day) << ", "
 			<< end_date.day << "/"
 			<< end_date.month << "/"
 			<< end_date.year << endl;
 
-	const short days = count_business_days_in_range(start_date, end_date);
-	cout << endl << "Actual vacation days is: " << days << endl;
+	cout << endl << "Period length is: " << calc_diff_days(start_date, end_date);
+	cout << endl << "Period length is (including first day): " << calc_diff_days(start_date, end_date, true);
 
 	return 0;
 }
