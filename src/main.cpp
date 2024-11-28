772e92f5d625414c8de4bfefca80b367b4a74995
Muhannad Elbolaky <think-basin-amber@duck.com>
2024-11-28 19:11:44 +0200

    solved

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,30 @@
+#include <cmath>
 #include <iostream>
 
-using namespace std;
+namespace {
+    int read_number() {
+        int number;
+        std::cout << "Please enter a number: ";
+        std::cin >> number;
+        return number;
+    }
+
+    double calc_area(int rect_a, int rect_d) {
+        return rect_a * std::sqrt(pow(rect_d, 2) - pow(rect_a, 2));
+    }
+
+    void print_result(double area) {
+        std::cout << "The area of the rectangle is " << area << std::endl;
+    }
+} // namespace
 
 int main() {
+    int rect_a = read_number();
+    int rect_d = read_number();
+
+    double area = calc_area(rect_a, rect_d);
+
+    print_result(area);
+
     return 0;
 }
