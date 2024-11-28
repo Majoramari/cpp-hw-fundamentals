713055b6fcdb4f4418d1bd489e51646386204903
Muhannad Elbolaky <think-basin-amber@duck.com>
2024-11-28 20:19:49 +0200

    solved

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,34 @@
+#include <cmath>
 #include <iostream>
 
-using namespace std;
+using std::cin;
+using std::cout;
+using std::endl;
+using std::string;
+
+namespace utils {
+    static int read_number(string message = "Please enter a number: ") {
+        int number = 0;
+        cout << message;
+        cin >> number;
+        return number;
+    }
+} // namespace utils
+
+namespace {
+    double calc_circle_area(int radius) { return M_PI * pow(radius, 2); }
+
+    void print_result(double area) {
+        cout << "The area of the circle is " << area << endl;
+    }
+} // namespace
 
 int main() {
+    int radius = utils::read_number("Please enter the radius of the circle: ");
+
+    double circle_area = calc_circle_area(radius);
+
+    print_result(circle_area);
+
     return 0;
 }
