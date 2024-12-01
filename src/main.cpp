c45fd7d5021d95b4b613086b3d2cb4383df839d1
Muhannad Elbolaky <think-basin-amber@duck.com>
2024-12-01 14:22:03 +0200

    solved

--- src/main.cpp
+++ src/main.cpp
@@ -1,1 +1,31 @@
-int main() { return 0; }
+#include <iostream>
+
+using std::cin;
+using std::cout;
+using std::endl;
+using std::string;
+
+namespace utils {
+    static int read_number(string message = "Please enter a number: ") {
+        int number;
+        cout << message;
+        cin >> number;
+        return number;
+    }
+} // namespace utils
+
+namespace {
+    void print_numbers_up_to(int number) {
+        for (int i = 1; i <= number; i++) {
+            cout << i << endl;
+        }
+    }
+} // namespace
+
+int main() {
+    int number = utils::read_number();
+
+    print_numbers_up_to(number);
+
+    return 0;
+}
