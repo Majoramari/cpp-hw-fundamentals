434b73fae455c3580f4374ca5470e4e8c2f809af
Muhannad Elbolaky <think-basin-amber@duck.com>
2024-12-10 09:19:00 +0200

    solve

--- src/main.cpp
+++ src/main.cpp
@@ -6,29 +6,44 @@ using std::cout;
 using std::endl;
 using std::string;
 
-class Person {
+class Employee {
+public:
+    enum Department { HR, FINANCE, SALES, MARKETING };
+
 private:
     int id_;
-    string first_name_, last_name_, email_, phone_number_;
+    string first_name_, last_name_, title_, email_, phone_number_;
+    float salary_;
+    Department department_;
 
 public:
-    Person(int identifier,
-           string first_name,
-           string last_name,
-           string email,
-           string phone_number) {
+    Employee(int identifier,
+             string first_name,
+             string last_name,
+             string title,
+             string email,
+             string phone_number,
+             float salary,
+             Department department) {
         id_ = identifier;
         first_name_ = first_name;
         last_name_ = last_name;
+        title_ = title;
         email_ = email;
         phone_number_ = phone_number;
+        salary_ = salary;
+        department_ = department;
     }
 
     [[nodiscard]] int id() const { return id_; }
 
     [[nodiscard]] string first_name() const { return first_name_; }
 
+    void set_first_name(string value) { first_name_ = value; }
+
     [[nodiscard]] string last_name() const { return last_name_; }
+
+    void set_last_name(string value) { last_name_ = value; }
 
     [[nodiscard]] string full_name() const {
         return first_name_ + " " + last_name_;
@@ -36,7 +51,34 @@ public:
 
     [[nodiscard]] string email() const { return email_; }
 
+    void set_email(string value) { email_ = value; }
+
+    [[nodiscard]] string title() const { return title_; }
+
+    void set_title(string value) { title_ = value; }
+
     [[nodiscard]] string phone_number() const { return phone_number_; }
+
+    void set_phone_number(string value) { phone_number_ = value; }
+
+    [[nodiscard]] float salary() const { return salary_; }
+
+    void set_salary(float value) { salary_ = value; }
+
+    [[nodiscard]] string department() const {
+        switch (department_) {
+            case HR:
+                return "Human Resources";
+            case FINANCE:
+                return "Finance";
+            case SALES:
+                return "Sales";
+            case MARKETING:
+                return "Marketing";
+        }
+    }
+
+    void set_department(Department value) { department_ = value; }
 
     void print() const {
         cout << "Info:" << endl;
@@ -45,8 +87,10 @@ public:
         cout << "First Name:   " << first_name() << endl;
         cout << "Last Name:    " << last_name() << endl;
         cout << "Full Name:    " << full_name() << endl;
+        cout << "Title:        " << title() << endl;
         cout << "Email:        " << email() << endl;
         cout << "Phone Number: " << phone_number() << endl;
+        cout << "Salary:       " << salary() << endl;
         cout << "______________" << endl;
     };
 
@@ -63,13 +107,22 @@ public:
     }
 };
 
+using Department = Employee::Department;
+
 int main() {
+    // Create an Employee object
+    Employee employee(1, "Muhannad", "Elbolaky", "Software Developer",
+                      "4u0yS@example.com", "+201997318", 120000.0,
+                      Department::HR);
 
-    Person person(10, "John", "Doe", "oY5Z0@example.com", "+1234567890");
-    person.print();
+    // Print the employee's information
+    employee.print();
 
-    person.send_sms("Hello");
-    person.send_email("Hi", "How are you?");
+    // Send an email to the employee
+    employee.send_email("You're fired!", "Goodbye");
+
+    // Send an SMS to the employee
+    employee.send_sms("Hello, It's me");
 
     return 0;
 }
