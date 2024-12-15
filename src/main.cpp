dd93d34999612072503b556fff0db97f3dc8a891
Muhannad Elbolaky <think-basin-amber@duck.com>
2024-12-15 21:13:41 +0200

    solved

--- src/main.cpp
+++ src/main.cpp
@@ -1,1 +1,38 @@
-int main() { return 0; }
+#include <iostream>
+
+using std::cin;
+using std::cout;
+using std::endl;
+using std::string;
+
+static int read_number(const string &message = "Please enter a number: ") {
+    int number = 0;
+    cout << message;
+    cin >> number;
+    return number;
+}
+
+static bool is_prime(int number) {
+    if (number <= 1) return false;
+
+
+    for (int i = 2; i <= (number / 2); i++) {
+        if (number % i == 0) return false;
+    }
+
+    return true;
+}
+
+static void print_primes_1_to_n(int number) {
+    for (int i = 1; i <= number; i++) {
+        if (is_prime(i)) {
+            cout << i << endl;
+        }
+    }
+}
+
+int main() {
+    int number = read_number();
+    print_primes_1_to_n(number);
+    return 0;
+}
