0ba6e2be16bdf6883206c493f5a7e58dd19bba6a
Muhannad Elbolaky <think-basin-amber@duck.com>
2024-12-15 15:51:20 +0200

    solve homework 2

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,14 @@
-#include <cmath>
 #include <iostream>
-#include <string>
 
-using std::cout;
-using std::endl;
-using std::string;
+using namespace std;
 
 class Person {
 private:
     int id_;
-    string first_name_, last_name_, email_, phone_number_;
+    string first_name_;
+    string last_name_;
+    string email_;
+    string phone_number_;
 
 public:
     Person(int identifier,
@@ -26,50 +25,189 @@ public:
 
     [[nodiscard]] int id() const { return id_; }
 
-    [[nodiscard]] string first_name() const { return first_name_; }
+    string first_name() { return first_name_; }
 
-    [[nodiscard]] string last_name() const { return last_name_; }
+    void set_first_name(string value) { first_name_ = value; }
 
-    [[nodiscard]] string full_name() const {
-        return first_name_ + " " + last_name_;
+    string last_name() { return last_name_; }
+
+    void set_last_name(string value) { last_name_ = value; }
+
+    string email() { return email_; }
+
+    void set_email(string value) { email_ = value; }
+
+    string phone_number() { return phone_number_; }
+
+    void set_phone_number(string value) { phone_number_ = value; }
+
+    string full_name() { return first_name_ + " " + last_name_; }
+
+    void print() {
+        cout << "Info:" << endl;
+        cout << "___________________" << endl;
+        cout << "ID           : " << id() << endl;
+        cout << "FirstName    : " << first_name() << endl;
+        cout << "LastName     : " << last_name() << endl;
+        cout << "Full Name    : " << full_name() << endl;
+        cout << "Email        : " << email() << endl;
+        cout << "Phone Number : " << phone_number() << endl;
+        cout << endl << "___________________" << endl;
     }
 
-    [[nodiscard]] string email() const { return email_; }
+    void send_email(string subject, string body) {
+        cout << "\nThe following message sent successfully to email: "
+             << email_;
+        cout << "\nSubject: " << subject;
+        cout << "\nBody: " << body << endl;
+    }
 
-    [[nodiscard]] string phone_number() const { return phone_number_; }
+    void send_sms(string text_message) {
+        cout << "\nThe following SMS sent successfully to phone: "
+             << phone_number_;
+        cout << "\n" << text_message << endl;
+    }
+};
 
-    void print() const {
+class Employee : public Person {
+public:
+    enum class Department { IT, FINANCE, MARKETING, HR, CEO };
+
+private:
+    string title_;
+    Department department_;
+    float salary_;
+
+public:
+    Employee(int identifier,
+             string first_name,
+             string last_name,
+             string email,
+             string phone_number,
+             string title,
+             Department department,
+             float salary) :
+        Person(identifier, first_name, last_name, email, phone_number) {
+        title_ = title;
+        department_ = department;
+        salary_ = salary;
+    }
+
+    string title() { return title_; }
+
+    void set_title(string value) { title_ = value; }
+
+    string department() {
+        switch (department_) {
+            case Department::IT:
+                return "IT";
+            case Department::FINANCE:
+                return "Finance";
+            case Department::MARKETING:
+                return "Marketing";
+            case Department::HR:
+                return "Hr";
+            case Department::CEO:
+                return "Ceo";
+        }
+    }
+
+    void set_department(Department value) { department_ = value; }
+
+    void set_salary(float salary) { salary_ = salary; }
+
+    [[nodiscard]] float salary() const { return salary_; }
+
+    void print() {
         cout << "Info:" << endl;
-        cout << "______________" << endl;
-        cout << "ID:           " << id() << endl;
-        cout << "First Name:   " << first_name() << endl;
-        cout << "Last Name:    " << last_name() << endl;
-        cout << "Full Name:    " << full_name() << endl;
-        cout << "Email:        " << email() << endl;
-        cout << "Phone Number: " << phone_number() << endl;
-        cout << "______________" << endl;
+        cout << "___________________" << endl;
+        cout << "ID           : " << id() << endl;
+        cout << "FirstName    : " << first_name() << endl;
+        cout << "LastName     : " << last_name() << endl;
+        cout << "Full Name    : " << full_name() << endl;
+        cout << "Email        : " << email() << endl;
+        cout << "Phone Number : " << phone_number() << endl;
+        cout << "Title        : " << title() << endl;
+        cout << "Department   : " << department() << endl;
+        cout << "Salary       : " << salary() << endl;
+        cout << endl << "___________________" << endl;
+    }
+};
+
+class Developer : public Employee {
+public:
+    enum class ProgrammingLanguage { JAVA, C, CPLUSPLUS, GOLANG, TYPESCRIPT };
+
+private:
+    ProgrammingLanguage main_programming_language_;
+
+public:
+    Developer(int identifier,
+              string first_name,
+              string last_name,
+              string email,
+              string phone_number,
+              string title,
+              Department department,
+              float salary,
+              ProgrammingLanguage programming_language) :
+        Employee(identifier,
+                 first_name,
+                 last_name,
+                 email,
+                 phone_number,
+                 title,
+                 department,
+                 salary) {
+        main_programming_language_ = programming_language;
     };
 
-    void send_email(string subject, string message) const {
-        cout << endl << "The message was sent to " << email() << endl;
-        cout << "Subject: " << subject << endl;
-        cout << "Message: " << message << endl;
+    string main_programming_language() {
+        switch (main_programming_language_) {
+            case ProgrammingLanguage::JAVA:
+                return "Java";
+            case ProgrammingLanguage::C:
+                return "C";
+            case ProgrammingLanguage::CPLUSPLUS:
+                return "C++";
+            case ProgrammingLanguage::GOLANG:
+                return "Go";
+            case ProgrammingLanguage::TYPESCRIPT:
+                return "TypeScript";
+        }
     }
 
-    void send_sms(string message) const {
-        cout << endl
-             << "Sending SMS to " << phone_number()
-             << " with message: " << message << endl;
+    void set_main_programming_language(ProgrammingLanguage value) {
+        main_programming_language_ = value;
+    }
+
+
+    void print() {
+        cout << "Info:" << endl;
+        cout << "___________________" << endl;
+        cout << "ID                        : " << id() << endl;
+        cout << "FirstName                 : " << first_name() << endl;
+        cout << "LastName                  : " << last_name() << endl;
+        cout << "Full Name                 : " << full_name() << endl;
+        cout << "Email                     : " << email() << endl;
+        cout << "Phone Number              : " << phone_number() << endl;
+        cout << "Title                     : " << title() << endl;
+        cout << "Department                : " << department() << endl;
+        cout << "Salary                    : " << salary() << endl;
+        cout << "Main Programming Language : " << main_programming_language()
+             << endl;
+        cout << "___________________" << endl;
     }
 };
 
 int main() {
+    Developer developer(1, "Muhannad", "Elbolaky", "j3z5d@example.com",
+                        "+201055555555", "Software Engineer",
+                        Employee::Department::IT, 50000,
+                        Developer::ProgrammingLanguage::GOLANG);
 
-    Person person(10, "John", "Doe", "oY5Z0@example.com", "+1234567890");
-    person.print();
-
-    person.send_sms("Hello");
-    person.send_email("Hi", "How are you?");
+    developer.print();
 
     return 0;
 }
+
