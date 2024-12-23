aa7633bb1643c09c3d70c7c8e9776060bb2c9483
Muhannad Elbolaky <think-basin-amber@duck.com>
2024-12-23 17:21:10 +0200

    solved

--- src/main.cpp
+++ src/main.cpp
@@ -1,1 +1,31 @@
-int main() { return 0; }
+#include <iostream>
+
+using std::cin;
+using std::cout;
+using std::endl;
+using std::string;
+
+static int read_number(string message = "Please enter a number: ") {
+    int number = 0;
+    cout << message;
+    cin >> number;
+    return number;
+}
+
+static void print_letters(const int number) {
+    for (int i = number; i >= 0; i--) {
+        for (int j = 0; j < i; j++) {
+            cout << char(i + 64);
+        }
+        cout << endl;
+    }
+}
+
+int main() {
+    int line_count = read_number();
+
+    cout << endl;
+    print_letters(line_count);
+
+    return 0;
+}
