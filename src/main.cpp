af04a00b3bb1a6c9c04ff44109bf9f1536dd9561
Muhannad Elbolaky <think-basin-amber@duck.com>
2025-04-24 00:41:15 +0200

    Add checks for client existence in main function
    Implemented logic to verify if specific clients exist using `Client::is_exist`. Added corresponding output messages to indicate existence or non-existence of clients. This improves the program's functionality by providing clear feedback on client availability.

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,18 @@
 #include <Client.hpp>
+#include <iostream>
 
 int main() {
 
     const Client client = Client::find("A101");
     client.print_info();
+
+    if (Client::is_exist("A101")) {
+        std::cout << "Client A101 already exist\n";
+    }
+
+    if (!Client::is_exist("X250")) {
+        std::cout << "Client X250 doesn't exist\n";
+    }
 
     return 0;
 }
