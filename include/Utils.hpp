04af4c23e90fe90643dbbafa7ded82ae77044c4a
Muhannad Elbolaky <think-basin-amber@duck.com>
2025-07-09 21:27:28 +0300

    ♻️ refactor(Utils): simplify type usage
    - Replace fully qualified names with using directives
    - Add new methods for printing separator and row

--- include/Utils.hpp
+++ include/Utils.hpp
@@ -1,13 +1,21 @@
 #ifndef UTILS_HPP
 #define UTILS_HPP
 
-#include <sstream>
+#include <initializer_list>
 #include <string>
 #include <vector>
 
+using std::initializer_list;
+using std::pair;
+using std::string;
+using std::vector;
+
 class Utils {
 public:
-    static std::vector<std::string> split(const std::string &str, const std::string &delim);
+    static vector<string> split(const string &str, const string &delim);
+    static void
+    print_separator(size_t width, bool new_line_before = true, bool new_line_after = true);
+    static void print_row( initializer_list<pair<string, int>> cols);
 };
 
-#endif //UTILS_HPP
+#endif // UTILS_HPP
--- src/libs/Utils.cpp
+++ src/libs/Utils.cpp
@@ -1,5 +1,9 @@
 #include "Utils.hpp"
+#include <iomanip>
+#include <iostream>
 
+using std::cout;
+using std::endl;
 using std::string;
 using std::vector;
 
@@ -15,4 +19,24 @@ vector<string> Utils::split(const string &str, const string &delim) {
 
     tokens.emplace_back(str.substr(start));
     return tokens;
+}
+
+void Utils::print_separator(size_t width, bool new_line_before, bool new_line_after) {
+    if (new_line_before) {
+        cout << endl;
+    }
+
+    cout << string(width, '_');
+
+    if (new_line_after) {
+        cout << endl;
+    }
+}
+
+void Utils::print_row(initializer_list<pair<string, int>> cols) {
+    std::cout << "| ";
+    for (const auto &[title, width]: cols) {
+        std::cout << std::left << std::setw(width) << title << " | ";
+    }
+    std::cout << "\n";
 }
